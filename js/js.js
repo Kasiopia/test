27e1d523ae0b38ce596d2006e65e31be20f8ee2b
Sergey Chernov <scout1982@mail.ru>
2018-08-27 18:41:48 +0200

    check commit

--- js/js.js
+++ js/js.js
@@ -3,28 +3,51 @@ var currentSlide = 0;
 var slideInterval = setInterval(nextSlide,2000);
 
 function nextSlide(){
-    goToSlide(currentSlide+1); 
-     
+	goToSlide(currentSlide+1); 
 }
-
 function previousSlide(){
-    goToSlide(currentSlide-1);
+	goToSlide(currentSlide-1);
 }
-
 function goToSlide(n){
-    slides[currentSlide].className = 'card-slide';
-    currentSlide = (n+slides.length)%slides.length; 
-    slides[currentSlide].className = 'card-slide card-slide--showing';    
+	slides[currentSlide].className = 'card-slide';
+	currentSlide = (n+slides.length)%slides.length; 
+	slides[currentSlide].className = 'card-slide card-slide--showing';    
 }
-
 var next = document.getElementById('next');
 var previous = document.getElementById('previous');
 
 next.onclick = function(){
 	clearInterval(slideInterval);   //останавливает слайдер по нажатию на next
-    nextSlide();
+	nextSlide();
 };
 previous.onclick = function(){
 	clearInterval(slideInterval);   //останавливает слайдер по нажатию на previous
-    previousSlide();
+	previousSlide();
 };
+
+var opencity = document.querySelector (".js-city");
+var city = document.querySelector (".modal-city");
+var closecity = document.querySelector (".modal-content-close-city");
+
+    opencity.addEventListener ("click", function (event) {
+      event.preventDefault();
+      city.classList.add ("modal-content-show-city");
+    });   
+
+    closecity.addEventListener ("click", function(event) {
+      event.preventDefault();
+      city.classList.remove ("modal-content-show-city")
+    });
+
+    window.addEventListener ("keydown", function(event) {
+      if (event.keyCode === 27) {
+        if (city.classList.contains ("modal-content-show-city")) {
+          city.classList.remove ("modal-content-show-city");
+        }
+      }
+    });
+
+$('.reviews-slider__wrappper').slick();
+$('.slider__wrapper').slick({
+	dots: true
+});
