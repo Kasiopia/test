0b811b4c6a7300ac11899de3a5ca86a6fc1f6573
Sergey Chernov <scout1982@mail.ru>
2018-09-01 15:22:09 +0300

    ne vishlo

--- js/js.js
+++ js/js.js
@@ -31,33 +31,33 @@ previous.onclick = function(){
 
 $('.reviews-slider__wrappper').slick();
 $('.slider__wrapper').slick({
-  dots: true,
-  responsive: [
-    {
-      breakpoint: 1280,
-      settings: {
-        dots: true,
-        arrows: false,
-        аccessibility: false
-      }
-    },
-    {
-      breakpoint: 700,
-      settings: {
-        dots: false,
-        arrows: false,
-        аccessibility: false
-      }
-    },
-    {
-      breakpoint: 700,
-      settings: {
-        dots: false,
-        arrows: true,
-        аccessibility: true
-      }
-    }
-  ]
+	dots: true,
+	responsive: [
+	{
+		breakpoint: 1280,
+		settings: {
+			dots: true,
+			arrows: false,
+			аccessibility: false
+		}
+	},
+	{
+		breakpoint: 700,
+		settings: {
+			dots: false,
+			arrows: false,
+			аccessibility: false
+		}
+	},
+	{
+		breakpoint: 700,
+		settings: {
+			dots: false,
+			arrows: true,
+			аccessibility: true
+		}
+	}
+	]
 });
 
 //подключение всплывающих окон
@@ -68,44 +68,44 @@ var closecity = document.querySelector (".modal-content-close");
 var modaloverlay = document.querySelector(".modal-overlay");
 
 opencity.addEventListener ("click", function (event) {
-  event.preventDefault();
-  city.classList.add ("modal-content-show");
-  modaloverlay.classList.add("modal-overlay--active");
+	event.preventDefault();
+	city.classList.add ("modal-content-show");
+	modaloverlay.classList.add("modal-overlay--active");
 });
 
 closecity.addEventListener ("click", function(event) {
-  event.preventDefault();
-  city.classList.remove ("modal-content-show")
-  modaloverlay.classList.remove("modal-overlay--active");
+	event.preventDefault();
+	city.classList.remove ("modal-content-show")
+	modaloverlay.classList.remove("modal-overlay--active");
 });
 
 window.addEventListener ("keydown", function(event) {
-  if (event.keyCode === 27) {
-    if (city.classList.contains ("modal-content-show")) {
-      city.classList.remove ("modal-content-show");
-      modaloverlay.classList.remove("modal-overlay");
-    }
-  }
+	if (event.keyCode === 27) {
+		if (city.classList.contains ("modal-content-show")) {
+			city.classList.remove ("modal-content-show");
+			modaloverlay.classList.remove("modal-overlay");
+		}
+	}
 });
 
 //toogle menu open or close
 
-	var navMain = document.querySelector (".header__inner");
-	var navToggle = document.querySelector (".nav-toggle");
-	var header = document.querySelector (".header");
-  var slider = document.querySelector(".slider");
-  var cardslider = document.querySelector(".card-slider");
+var navMain = document.querySelector (".header__inner");
+var navToggle = document.querySelector (".nav-toggle");
+var header = document.querySelector (".header");
+var slider = document.querySelector(".slider");
+var cardslider = document.querySelector(".card-slider");
 
 
 //not js
-	navMain.classList.remove("header__inner--nojs");
-  slider.classList.remove("slider--nojs");
-  cardslider.classList.remove("card-slider--nojs");
+navMain.classList.remove("header__inner--nojs");
+slider.classList.remove("slider--nojs");
+cardslider.classList.remove("card-slider--nojs");
 
-	navToggle.addEventListener ("click", function(event) {
-		if (navMain.classList.contains("header__inner--opened")) {
-			navMain.classList.remove("header__inner--opened");
-		} else {
-			navMain.classList.add("header__inner--opened");
-		}
-	});
+navToggle.addEventListener ("click", function(event) {
+	if (navMain.classList.contains("header__inner--opened")) {
+		navMain.classList.remove("header__inner--opened");
+	} else {
+		navMain.classList.add("header__inner--opened");
+	}
+});
