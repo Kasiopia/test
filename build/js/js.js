d2ad795f90203aa6a9b63f4c074cb5b5c661a84d
Sergey Chernov <scout1982@mail.ru>
2018-09-02 21:36:06 +0300

    new ветка улучшение card секции

--- build/js/js.js
+++ build/js/js.js
@@ -1,35 +1,4 @@
-//подключение слайер карточки
-
-var slides = document.querySelectorAll('#card-slider__wrapper--2 .card-slide');
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,2000);
-
-function nextSlide(){
-	goToSlide(currentSlide+1);
-}
-function previousSlide(){
-	goToSlide(currentSlide-1);
-}
-function goToSlide(n){
-	slides[currentSlide].className = 'card-slide';
-	currentSlide = (n+slides.length)%slides.length;
-	slides[currentSlide].className = 'card-slide card-slide--showing';
-}
-var next = document.getElementById('next--2');
-var previous = document.getElementById('prev--2');
-
-next.onclick = function(){
-	clearInterval(slideInterval);   //останавливает слайдер по нажатию на next
-	nextSlide();
-};
-previous.onclick = function(){
-	clearInterval(slideInterval);   //останавливает слайдер по нажатию на previous
-	previousSlide();
-};
-
-//подключение основного слайер и слайдера отзывы
-
-$('.reviews-slider__wrappper').slick();
+//подключение слайдеров
 $('.slider__wrapper').slick({
 	dots: true,
 	responsive: [
@@ -59,7 +28,13 @@ $('.slider__wrapper').slick({
 	}
 	]
 });
-
+$('.card-slider--1').slick();
+$('.card-slider--2').slick();
+$('.card-slider--3').slick();
+$('.card-slider--4').slick();
+$('.card-slider--5').slick();
+$('.card-slider--6').slick();
+$('.reviews-slider__wrapper').slick();
 //подключение всплывающих окон и закрытие окон
 
 var opencity = document.querySelector (".js-city");
@@ -98,13 +73,10 @@ window.addEventListener ("keydown", function(event) {
 });
 
 //not js
-
 navMain.classList.remove("header__inner--nojs");
 slider.classList.remove("slider--nojs");
-cardslider.classList.remove("card-slider--nojs");
 
 // реакция на нажатие в nav-toggle
-
 navToggle.addEventListener ("click", function(event) {
 	if (navMain.classList.contains("header__inner--opened")) {
 		navMain.classList.remove("header__inner--opened");
@@ -114,7 +86,6 @@ navToggle.addEventListener ("click", function(event) {
 });
 
 // реакция на нажатие в dropdown-toggle
-
 subToggle.addEventListener ("click", function(event) {
 	if (subnavMain.classList.contains("main-nav--opened")) {
 		subnavMain.classList.remove("main-nav--opened");
@@ -122,6 +93,25 @@ subToggle.addEventListener ("click", function(event) {
 		subnavMain.classList.add("main-nav--opened");
 	}
 });
+
+// реакция на нажатие на card
+var card = document.querySelector (".card");
+//var cardToggle = document.querySelector (".card--click");
+
+
+card.addEventListener ("click", function (event) {
+	event.preventDefault();
+	card.classList.add ("card--click");
+});
+
+//cardToggle.addEventListener ("click", function(event) {
+//	if (card.classList.contains("card--click")) {
+//		card.classList.remove("card--click");
+//	} else {
+//		card.classList.add("card--click");
+//	}
+//});
+
 //закрываем sub меню dropdown
 
 closeDropMenu.addEventListener ("click", function(event) {
@@ -138,3 +128,4 @@ window.addEventListener ("keydown", function(event) {
 		}
 	}
 });
+
